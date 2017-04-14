48f1d0e68ccfe86c9d8ae1cb1012eef974a548a1
lenny <lenny09918050@googlemail.com>
2017-04-14 11:46:49 +0200

    added todo's^^

--- templetizor.js
+++ templetizor.js
@@ -1,3 +1,15 @@
 /**
  * Created by lenny on 14.04.17.
  */
+//TODO: read in static file
+
+//TODO: parse content to figure out texts
+
+//TODO: save texts in JSON
+
+//TODO: exchange text with mustached keys for it
+
+//TODO: save JSON file
+
+//TODO: save templated .mustache file
+
