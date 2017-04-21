762bfcc221855ddbf0844c43c760233ce1f49045
Marko <markostojiljkovic1943@gmail.com>
2017-04-21 03:03:12 +0200

    removed ids that have no text

--- templetizor.js
+++ templetizor.js
@@ -4,19 +4,36 @@ var cheerio = require('cheerio');
 var $ = cheerio.load(fs.readFileSync('simpleSample.html'));
 
 
-var arr = [];
+var temparray = [];
+var array = [];
 
-// the thing we need to deal now with is the duplicating of text cause of nested parent-child divs
+
 $('[id]').filter(function() {
     var temp = {
         id: $(this).attr("id"),
         text: $(this).clone().children().remove().end().text().replace(/\s+/g, " ").trim()
     }
-    arr.push(temp);
+    temparray.push(temp);
 
 });
 
-var myJsonString = JSON.stringify(arr, null, 4);
+// did this so I can remove the id's that have no text
+// we could probably figure out a better way to do this since im using 2 arrays this way but fokit
+for (var i = 0; i <= temparray.length - 1; i++) {
+    if (temparray[i].text) {
+        var ids = temparray[i].id;
+        var texts = temparray[i].text;
+
+        var temp = {
+            id: ids,
+            text: texts
+        }
+
+        array.push(temp);
+    }
+}
+
+var myJsonString = JSON.stringify(array, null, 4);
 console.log(myJsonString)
 
 fs.writeFile('sample.json', myJsonString, (err) => {
